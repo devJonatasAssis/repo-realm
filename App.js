c44dc40649a0e48a0f2edd304cda1c3d2c0fb276
Jonatas Assis <jonatasassis@Jonatass-MacBook-Pro.local>
2020-09-02 21:53:53 -0300

    Aplicação com Realm

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Main from './src/pages/Main'
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" translucent />
+      <Main />
+    </>
+  );
+}
